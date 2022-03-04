395113916685cf9a8b150674faf4c8d4b1ea9bfb
Dallen Baldwin <dallen.baldwin@gmail.com>
2022-03-03 22:19:04 -0700

    consolidate navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,14 +1,11 @@
 <script setup lang="ts">
-import { Icon } from '@vicons/utils';
-import { DarkModeRound, LightModeRound, AccountCircleRound } from '@vicons/material';
-import { useDarkStore } from '@/stores/dark';
 import { storeToRefs } from 'pinia';
 import { View } from '@/router/index';
 import { useUserStore } from '@/stores/user';
+import UserMenu from './UserMenu.vue';
+import LightDark from './LightDark.vue';
 
-const { dark } = storeToRefs(useDarkStore());
 const { user } = storeToRefs(useUserStore());
-const { toggleDark } = useDarkStore();
 </script>
 
 <template>
@@ -31,15 +28,8 @@ const { toggleDark } = useDarkStore();
       <RouterLink class="font-semibold" to="/">Home</RouterLink>
       <RouterLink class="font-semibold" :to="`/${View.MEALS}`">Meals</RouterLink>
       <RouterLink class="font-semibold" :to="`/${View.ABOUT}`">About</RouterLink>
-      <div class="text-dark-primary dark:text-light-primary">
-        <Icon class="cursor-pointer" @click="toggleDark" size="32">
-          <LightModeRound v-if="dark" />
-          <DarkModeRound v-else />
-        </Icon>
-        <Icon size="32" v-if="user !== undefined">
-          <AccountCircleRound />
-        </Icon>
-      </div>
+      <UserMenu v-if="!!user" />
+      <LightDark v-else />
     </div>
   </nav>
 </template>
