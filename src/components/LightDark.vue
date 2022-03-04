16c3a3f5c02cea6a4a2997f2d920d649c45f5f0b
Dallen Baldwin <dallen.baldwin@gmail.com>
2022-03-03 22:17:10 -0700

    add theme switcher component

---
+++ src/components/LightDark.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { useDarkStore } from '@/stores/dark';
+import { storeToRefs } from 'pinia';
+import { Icon } from '@vicons/utils';
+import { LightModeRound, DarkModeRound } from '@vicons/material';
+
+const { dark } = storeToRefs(useDarkStore());
+const { toggleDark } = useDarkStore();
+</script>
+
+<template>
+  <Icon
+    class="cursor-pointer hover:opacity-75 duration-300"
+    @click="toggleDark"
+    size="32"
+  >
+    <LightModeRound v-if="dark" />
+    <DarkModeRound v-else />
+  </Icon>
+</template>
