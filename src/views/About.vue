81dc588424d1c557ba684f2860a0d2089aa8038e
Dallen Baldwin <dallen.baldwin@gmail.com>
2022-03-05 15:57:51 -0700

    get started on about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,3 +1,14 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import H1 from '../components/typography/H1.vue';
+import H3 from '../components/typography/H3.vue';
+import P from '../components/typography/P.vue';
+</script>
 
-<template>About Page</template>
+<template>
+  <H1 text="About" />
+  <P>
+    We Need to Eat is a simple meal planner I built because my wife and I have a really
+    hard time picking what to eat sometimes.
+  </P>
+  <H3 text="Account" />
+</template>
