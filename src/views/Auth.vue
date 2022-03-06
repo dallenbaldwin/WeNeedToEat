57c4fd4b7790b1a20ee8bb10a672f579d060aab5
Dallen Baldwin <dallen.baldwin@gmail.com>
2022-03-05 22:01:42 -0700

    finish auth for now

--- src/views/Auth.vue
+++ src/views/Auth.vue
@@ -1,16 +1,26 @@
 <script setup lang="ts">
 import { useUserStore } from '@/stores/user';
-import { storeToRefs } from 'pinia';
-import { ref } from 'vue';
+import { computed, ref } from 'vue';
 import Button from '../components/Button.vue';
 import H1 from '@/components/typography/H1.vue';
 import Password from '../components/inputs/Password.vue';
 import Email from '../components/inputs/Email.vue';
+import P from '../components/typography/P.vue';
+import Fade from '../components/transitions/Fade.vue';
 
 const isLogin = ref(true);
 const email = ref('');
 const password = ref('');
+const verify = ref('');
 const { login, register } = useUserStore();
+const match = computed(() =>
+  !isLogin.value && password.value.length > 0 ? password.value === verify.value : true
+);
+const toggleType = () => {
+  isLogin.value = !isLogin.value;
+  password.value = '';
+  verify.value = '';
+};
 </script>
 
 <template>
@@ -20,20 +30,26 @@ const { login, register } = useUserStore();
     <div class="w-1/2">
       <Email label="Email" v-model="email" />
       <Password label="Password" v-model="password" />
-      <!-- TODO add second password for register -->
+      <Password label="Verify Password" v-model="verify" v-if="!isLogin" />
+      <Fade>
+        <P v-if="!match" class="text-danger-light dark:text-danger-dark">
+          Passwords must match!
+        </P>
+      </Fade>
       <div class="flex justify-end">
         <Button
           :text="isLogin ? 'Register' : 'Login'"
           class="mr-auto justify-self-start"
-          @click="() => (isLogin = !isLogin)"
+          @click="toggleType"
           secondary
         />
-        <div v-if="isLogin">
-          <Button @click="() => login(email, password)" text="Login" />
-        </div>
-        <div v-else>
-          <Button @click="() => register(email, password)" text="Register" />
-        </div>
+        <Button v-if="isLogin" @click="() => login(email, password)" text="Login" />
+        <Button
+          :disabled="!match"
+          v-else
+          @click="() => register(email, password)"
+          text="Register"
+        />
       </div>
     </div>
   </div>
