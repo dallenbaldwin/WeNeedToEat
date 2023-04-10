5c6f92f574e7c112eecfb2dd6bf174be523b16e8
Dallen Baldwin <dallen.baldwin@gmail.com>
2023-04-09 22:23:22 -0600

    move things around

---
+++ js/index.js
@@ -0,0 +1,93 @@
+function pillTML(tag) {
+  if (tag.length === 0) return;
+  return `<li class="c-tag-pill">
+         <span class="c-tag-pill-data">${tag}</span>
+         ${
+           tag === 'Restaurant' || tag === 'Cook at Home'
+             ? ''
+             : `<span class="c-tag-pill-delete">&times;</span>`
+         }
+      </li>`;
+}
+
+function addTag() {
+  let newTags = $('#tagInput')
+    .val()
+    .split(',')
+    .map(x => x.trim())
+    .filter(x => x.length > 0);
+  newTags.forEach(tag => {
+    $('#filterTagList').append(pillTML(tag));
+  });
+  $('.c-tag-pill-delete').click(killPill);
+  $('#tagInput').val('');
+  // add new tags to the existing tags
+  let tempDB = window.sessionStorage;
+  let currentTags = tempDB.getItem('tags');
+  if (currentTags === null) {
+    tempDB.setItem('tags', JSON.stringify(newTags));
+  } else {
+    let totalTags = JSON.parse(currentTags).concat(newTags);
+    tempDB.setItem('tags', JSON.stringify(totalTags));
+  }
+}
+
+function killPill() {
+  let tag = $(this).parent().find('.c-tag-pill-data')[0].innerText;
+  let tempDB = window.sessionStorage;
+  let tags = JSON.parse(tempDB.getItem('tags'));
+  tempDB.setItem('tags', JSON.stringify(tags.filter(x => x !== tag)));
+  $(this).parent().remove();
+}
+
+function decideToEat() {
+  let db = Object.values(window.localStorage).map(x => JSON.parse(x));
+  let tempDB = window.sessionStorage;
+  let tags = JSON.parse(tempDB.getItem('tags'));
+  let type = tempDB.getItem('innout').toLowerCase();
+  // remove entries that aren't the selected type
+  let results = db.filter(entry =>
+    entry.tags.map(tag => tag.toLowerCase()).includes(type)
+  );
+  // remove entries that don't have the desired tags
+  if (tags && tags.length > 0)
+    results = results.filter(x => x.tags.some(y => tags.includes(y)));
+
+  let rand =
+    results.map(x => x.name)[Math.floor(Math.random() * results.length)] || 'Nothing...';
+  // <h1 class="display-4"></h1>
+
+  $('#resultsArea').empty().append(`<h1 class="display-4">${rand}</h1>`);
+  if (rand === 'Nothing...')
+    $('#resultsArea').append(`<p>No meals matched the type and tags</p>`);
+}
+
+function eatIn() {
+  $('#eatInBtn').removeClass('btn-outline-primary').addClass('btn-primary');
+  $('#eatOutBtn').removeClass('btn-primary').addClass('btn-outline-primary');
+  window.sessionStorage.setItem('innout', 'Cook at Home');
+}
+
+function eatOut() {
+  $('#eatOutBtn').removeClass('btn-outline-primary').addClass('btn-primary');
+  $('#eatInBtn').removeClass('btn-primary').addClass('btn-outline-primary');
+  window.sessionStorage.setItem('innout', 'Restaurant');
+}
+
+function getInnout() {
+  let innout = window.sessionStorage.getItem('innout');
+  if (innout === null) {
+    eatIn();
+  } else if (innout === 'Cook at Home') {
+    eatIn();
+  } else if (innout === 'Restaurant') {
+    eatOut();
+  }
+}
+
+function populateTags() {
+  let tags = window.sessionStorage.getItem('tags');
+  if (tags === null) return;
+  JSON.parse(tags).forEach(tag => $('#filterTagList').append(pillTML(tag)));
+  $('.c-tag-pill-delete').click(killPill);
+}
---
+++ js/meals.js
@@ -0,0 +1,99 @@
+function addMealOption() {
+  if ($('#mealInput').val() === '' || $('#typeSelect').val() === '') return;
+  let type = $('#typeSelect').val();
+  let tags = $('#tagInput')
+    .val()
+    .split(',')
+    .map(x => x.trim())
+    .filter(x => x.length > 0);
+  tags.push(type === 'in' ? 'Cook at Home' : 'Restaurant');
+  let row = {
+    name: $('#mealInput').val(),
+    tags: tags,
+  };
+  window.localStorage.setItem(row.name, JSON.stringify(row));
+  location.reload();
+}
+
+function deleteMeal() {
+  let key = $('#modalMealNameInput').attr('placeholder');
+  window.localStorage.removeItem(key);
+  location.reload();
+}
+
+function clearLocalStorage() {
+  $('#clearDBModal').modal('toggle');
+}
+
+function confirmClearLocalStorage() {
+  window.localStorage.clear();
+  location.reload();
+}
+
+function loadTable() {
+  let db = Object.values(window.localStorage).map(x => JSON.parse(x));
+  let table = $('#mealsTable');
+  table.empty();
+  db.forEach(row => {
+    table.append(`<tr class="c-clickable">
+            <td>${row.name}</td>
+            <td>${row.tags.join(', ')}</td>
+         </tr>`);
+  });
+}
+
+function pillTML(tag) {
+  if (tag.length === 0) return;
+  return `<li class="c-tag-pill">
+         <span class="c-tag-pill-data">${tag}</span>
+         ${
+           tag === 'Restaurant' || tag === 'Cook at Home'
+             ? ''
+             : `<span class="c-tag-pill-delete">&times;</span>`
+         }
+      </li>`;
+}
+
+function addTag() {
+  let newTags = $('#modalAddTagInput').val().split(',');
+  newTags.forEach(tag => {
+    $('#editModalTagList').append(pillTML(tag));
+  });
+  $('.c-tag-pill-delete').click(killPill);
+  $('#modalAddTagInput').val('');
+}
+
+function killPill() {
+  $(this).parent().remove();
+}
+
+function editRow() {
+  $('#editRecordModal').modal('toggle');
+  let mealData = JSON.parse(window.localStorage.getItem($(this).children()[0].innerText));
+  $('#modalMealNameInput').attr('placeholder', mealData.name);
+  let list = $('#editModalTagList');
+  list.empty();
+  mealData.tags.forEach(tag => {
+    $('#editModalTagList').append(pillTML(tag));
+  });
+  $('.c-tag-pill-delete').click(killPill);
+}
+
+function saveEdits() {
+  let input = $('#modalMealNameInput');
+  let mealName = input.val() === '' ? input.attr('placeholder') : input.val();
+  let tags = [];
+  $('#editModalTagList')
+    .find('.c-tag-pill-data')
+    .each(function () {
+      tags.push($(this).text());
+    });
+  let newData = {
+    name: mealName,
+    tags: tags,
+  };
+  let db = window.localStorage;
+  db.removeItem(input.attr('placeholder'));
+  db.setItem(mealName, JSON.stringify(newData));
+  location.reload();
+}
